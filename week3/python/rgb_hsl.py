8c50bfe15e50c767387c84100d4cc2f291aa04e7
18521368 <80139536+18521368@users.noreply.github.com>
2021-03-26 20:42:23 +0700

    Update rgb_hsl.py

--- week3/python/rgb_hsl.py
+++ week3/python/rgb_hsl.py
@@ -8,7 +8,7 @@ for i in range(len(x)):
     g = int(x[i][8:16],2)/255.0
     r = int(x[i][0:8],2)/255.0
     h, l, s = colorsys.rgb_to_hls(r, g, b)
-    filee.write(str(round(h*360,2))+'\t')
+    filee.write(str(round(h*360))+'\t')
 
     filee.write(str(round(s * 100, 2))+'\t')
 

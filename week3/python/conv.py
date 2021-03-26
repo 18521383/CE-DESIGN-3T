79ef33d12e9425149aef1389b13c816997cb4396
18521368 <80139536+18521368@users.noreply.github.com>
2021-03-26 19:43:03 +0700

    Add files via upload

---
+++ week3/python/conv.py
@@ -0,0 +1,15 @@
+# Read the image file.
+from scipy import misc
+import imageio
+data = imageio.imread('image.jpg')
+
+# Make a string with the format you want.
+text = ''
+for row in data:
+    for e in row:
+        text += '{}{}{}\n'.format(f'{e[0]:08b}', f'{e[1]:08b}', f'{e[2]:08b}')
+
+
+# Write the string to a file.
+with open('image.txt', 'w') as f:
+    f.write(text)

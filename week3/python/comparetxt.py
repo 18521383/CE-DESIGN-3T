a775a5f3a4472e25a256e321a5ed6d2f49d51331
18521383 <80138622+18521383@users.noreply.github.com>
2021-03-26 21:15:52 +0700

    Add files via upload

---
+++ week3/python/comparetxt.py
@@ -0,0 +1,29 @@
+import difflib
+import re
+#open output python and handling
+f=open ('python_rgb_hsl.txt','r')
+result1 =''
+for lines in f:
+    data = re.sub(r'\s+', '', lines)
+    result1 += data +"\n"
+text1liness =result1.splitlines(1)
+#open output hdl and handling
+f1=open ('FP32bit_float.txt','r')
+result2 =''
+for lines in f1:
+    data = re.sub(r'\s+', '', lines)
+    result2 +=data +'\n'
+text2liness =result2.splitlines(1)
+
+counter_line =0 #count line
+line_err=0  #Line numbers different between text1 and text2
+diffInstance = difflib.Differ()
+diffList = list(diffInstance.compare(text1liness, text2liness))
+for line in diffList:
+    counter_line +=1
+    if line[0] == '-':
+        line_err+=1
+print ('-'*50)
+print ("Comparison results (exact):",)
+result=((counter_line-line_err)/counter_line)*100
+print(round(result,2))

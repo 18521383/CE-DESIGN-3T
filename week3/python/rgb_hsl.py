b92a165392d99afa975d05411703eb58f1b58706
18521368 <80139536+18521368@users.noreply.github.com>
2021-03-26 20:37:11 +0700

    Update rgb_hsl.py

--- week3/python/rgb_hsl.py
+++ week3/python/rgb_hsl.py
@@ -3,11 +3,9 @@ file = open("rgb.txt","r")
 filee = open("hsl.txt","w")
 x = file.read()
 x = x.splitlines()
-def format(value):
-    return "%.3f" % value
 for i in range(len(x)):
     r = int(x[i][16:24],2)/255.0
-    g = int(x[i][16:24],2)/255.0
+    g = int(x[i][8:16],2)/255.0
     b = int(x[i][0:8],2)/255.0
     h, l, s = colorsys.rgb_to_hls(r, g, b)
     filee.write(str(round(h*360,2))+'\t')

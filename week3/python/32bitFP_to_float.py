da36f83562825f4bfae733920d930d1e314c98df
18521383 <80138622+18521383@users.noreply.github.com>
2021-03-26 22:24:07 +0700

    Update 32bitFP_to_float.py

--- week3/python/32bitFP_to_float.py
+++ week3/python/32bitFP_to_float.py
@@ -3,10 +3,10 @@ global i
 def FloatingPoint_to_Float(value_hex,x):
     f = int(value_hex, 16)
     if (i>0):
-        return (round((struct.unpack('f', struct.pack('I', f))[0]),x))
+        return (round(100*(struct.unpack('f', struct.pack('I', f))[0]),x))
     elif (i==0):
         return (round((struct.unpack('f', struct.pack('I', f))[0])))
-f=open ('rgb.txt','r')
+f=open ('rgb_hex.txt','r')
 temp =''
 for line in f:
     check =0
@@ -20,12 +20,16 @@ for line in f:
             temp+= str(FloatingPoint_to_Float(column,0)) +'\t'
             i+=1
         elif(i>0):
-            temp+= str(FloatingPoint_to_Float(column,4)) +'\t'
-    if(check==0):
+            temp+= str(FloatingPoint_to_Float(column,2)) +'\t'
+    if (check ==1):
+        break
+    elif(check==0):
         temp+='\n'
 #print (temp)
-with open('final.txt', 'w') as f:
+with open('FP32bit_float.txt', 'w') as f:
     f.write(temp)
+
+
 
 
     

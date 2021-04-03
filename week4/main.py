de78d0540dd73466acf0f3b5bb53b156493fce29
18521383 <80138622+18521383@users.noreply.github.com>
2021-04-03 12:52:07 +0700

    Update main.py

--- week4/main.py
+++ week4/main.py
@@ -1,13 +1,11 @@
 import os
 #convert image to binary text file
-os. system('python3 /home/ngocthai18521383/CE434.L21/week3/conv.py')
+os. system('python3 /home/ngocthai18521383/CE434.L21/week4/conv.py')
 #compile hdl
-os.system ('cd /home/ngocthai18521383/CE434.L21/week3')
-os.system ('vlog Addition_Subtraction.v compare.v convert.v Counter.v except.v fpu.v IMEM.v Multiplication.v pc.v post_norm.v pre_norm.v pre_norm_fmul.v primitives.v priority_encoder.v RGB_to_HSL.v testbench.v')
+os.system ('cd /home/ngocthai18521383/CE434.L21/week4')
+os.system ('vlog Addition_Subtraction.v compare.v Rounding.v FP32_To_Int.v convert.v Counter.v except.v fpu.v IMEM.v Multiplication.v pc.v post_norm.v pre_norm.v pre_norm_fmul.v primitives.v priority_encoder.v RGB_to_HSL.v testbench.v')
 #run simulation
 os.system ('vsim -c -do "run -all" testbench')
-#convert output from HDL (floating point 32bit) to float
-os.system ('python3 32bitFP_to_float.py')
 #Convert RGB (text file) to HSL: using python
 os.system ('python3 rgb_hsl.py')
 #compare result between python and hdl
